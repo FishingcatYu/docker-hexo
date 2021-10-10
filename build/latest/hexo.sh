cb8d1f6037929fe15c38ea8336dfa30690432ca5
FishingcatYu <62251573+FishingcatYu@users.noreply.github.com>
2021-10-10 21:54:35 +0800

    Update hexo.sh

--- build/latest/hexo.sh
+++ build/latest/hexo.sh
@@ -5,6 +5,7 @@ if [ -z "$(ls)" ]; then
 	hexo init ./
 	yarn remove hexo-renderer-stylus
 	yarn global add hexo-renderer-stylus
+	yarn add hexo-deployer-git --save
 fi
 
 # Populate arguments based on environment variables
