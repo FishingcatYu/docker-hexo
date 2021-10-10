6c3a0faed93679e01e14ad2fab66549c4a231892
FishingcatYu <62251573+FishingcatYu@users.noreply.github.com>
2021-10-10 20:41:46 +0800

    Update hexo.sh

--- build/latest/hexo.sh
+++ build/latest/hexo.sh
@@ -3,6 +3,8 @@
 # Initialize blog if root folder is empty
 if [ -z "$(ls)" ]; then
 	hexo init ./
+	yarn remove hexo-renderer-stylus
+	yarn global add hexo-renderer-stylus
 fi
 
 # Populate arguments based on environment variables
