136245ede5116dd135a92b815fb25a71aaf984a4
FishingcatYu <62251573+FishingcatYu@users.noreply.github.com>
2021-10-11 14:32:07 +0800

    Update hexo.sh

--- build/latest/hexo.sh
+++ build/latest/hexo.sh
@@ -6,8 +6,6 @@ if [ -z "$(ls)" ]; then
 	#cp /home/hexo/.docker/package.json /home/hexo/.hexo/package.json
 	npm install hexo-deployer-git --save
 	npm install hexo-admin --save
-	npm install minimatch@"3.0.2"  
-        npm update -d
 fi
 
 # Populate arguments based on environment variables
