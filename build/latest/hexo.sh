610f77086b884aa0451513c686f1b1a962686e60
FishingcatYu <62251573+FishingcatYu@users.noreply.github.com>
2021-10-11 11:38:47 +0800

    Update hexo.sh

--- build/latest/hexo.sh
+++ build/latest/hexo.sh
@@ -3,11 +3,12 @@
 # Initialize blog if root folder is empty
 if [ -z "$(ls)" ]; then
 	hexo init ./
-	cp /home/hexo/.docker/package.json /home/hexo/.hexo/package.json
+	#cp /home/hexo/.docker/package.json /home/hexo/.hexo/package.json
 	npm install hexo-deployer-git --save
+	npm install hexo-admin --save
 fi
 
 # Populate arguments based on environment variables
 hexo cl
 hexo g
-hexo server -i -s
+hexo s
