3d8dbbe2347b7d9e9f06141aecc6f8049d9e7b8b
FishingcatYu <62251573+FishingcatYu@users.noreply.github.com>
2021-10-11 12:13:15 +0800

    Update hexo.sh

--- build/latest/hexo.sh
+++ build/latest/hexo.sh
@@ -5,6 +5,7 @@ if [ -z "$(ls)" ]; then
 	hexo init ./
 	#cp /home/hexo/.docker/package.json /home/hexo/.hexo/package.json
 	npm install hexo-deployer-git --save
+	npm install hexo-admin --save
 fi
 
 # Populate arguments based on environment variables
